84a662daf3e41a2527c99e059eba269a54a74fdd
bilalelwan <elwanbilalpro@gmail.com>
2024-10-21 11:50:49 +0300

    adding lyrics.py

---
+++ lyrics-project/lyrics.py
@@ -0,0 +1,9 @@
+print("fill the blank")
+counter = 0
+while True:
+    print("I will never ____ you down")
+    counter+=1
+    answer = input("WORD?: ")
+    if answer == "let":
+        break
+print("well done, YOU attempts in", counter, )
